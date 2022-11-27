ea3e2c01ac8c8512514c89889e1b6c10db4697fa
Mikokrsk <mikokrsk@gmail.com>
2022-11-27 14:53:35 +0200

    Added HP to the enemy

--- 3D_Test_GAME/Assets/HP.cs
+++ 3D_Test_GAME/Assets/HP.cs
@@ -8,18 +8,28 @@ public class HP : MonoBehaviour
 {
     public Text text;
     public Transform cam;
-
+    public Transform player;
+    public Transform cub;
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        text.text = "11111111111";
+        var vec = player.position - cub.position;
+        if(vec.sqrMagnitude < 100)
+        {
+            text.enabled = true;
+       text.text = "11111111111";
         transform.LookAt(transform.position + cam.forward);
+        }
+        else
+        {
+            text.enabled = false;
+        }
     }
     
   //  SceneManager.LoadScene(0);
